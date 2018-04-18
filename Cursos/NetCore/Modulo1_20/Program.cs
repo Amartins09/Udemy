923f66b1aef257cf18eb6414ce1a9e850a272779
Adriano <amartins09@gmail.com>
2018-04-18 00:16:29 -0300

    Modulo 1 2.0

---
+++ Cursos/NetCore/Modulo1_20/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Modulo1_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .Configure(
+                    app => {
+                        app.Run(context => context.Response.WriteAsync("Ola Mundo2"));
+                    }
+                )
+                .Build();
+
+            host.Run();
+        }
+    }
+}
