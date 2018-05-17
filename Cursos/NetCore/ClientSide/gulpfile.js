6ee94c83b322e19f0abd40f19f0c0b300b6daf66
Adriano <amartins09@gmail.com>
2018-05-17 00:57:56 -0300

    Incluir e configurar o Browser-Sync

--- Cursos/NetCore/ClientSide/gulpfile.js
+++ Cursos/NetCore/ClientSide/gulpfile.js
@@ -2,13 +2,26 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var cssmin = require('gulp-cssmin');
 var uncss = require('gulp-uncss');
+var browser = require('browser-sync').create();
+
+gulp.task('browser', function(){
+    browser.init({
+        proxy: 'localhost:5000'
+    })
+
+    gulp.watch('./styles/*.css', ['css']);
+
+    gulp.watch('./js/*.js', ['js']);
+})
 
 gulp.task('js', function(){
     return gulp.src([
         './node_modules/bootstrap/dist/js/bootstrap.min.js',
         './node_modules/jquery/dist/jquery.min.js',
+        './js/site.js'
     ])
-    .pipe(gulp.dest('wwwroot/js/'));
+    .pipe(gulp.dest('wwwroot/js/'))
+    .pipe(browser.stream());
 });
 
 gulp.task('css', function(){
@@ -19,9 +32,14 @@ gulp.task('css', function(){
     .pipe(concat('site.min.css'))
     .pipe(cssmin())
     .pipe(uncss({html: ['Views/**/*.cshtml']}))
-    .pipe(gulp.dest('wwwroot/css/'));
+    .pipe(gulp.dest('wwwroot/css/'))
+    .pipe(browser.stream());
 });
 
 gulp.task('watch-css', function(){
     gulp.watch('./styles/*.css', ['css']);
+});
+
+gulp.task('watch-js', function(){
+    gulp.watch('./js/*.js', ['js']);
 });
