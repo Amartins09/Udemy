099986f2188dbecff2998fa79168c007eb18dcc2
Adriano <amartins09@gmail.com>
2018-05-22 00:47:04 -0300

    Criar projeto Web -> Category

---
+++ Cursos/NetCore/LojaConstrucao/src/Loja.Web/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Loja.Web.Models;
+
+namespace Loja.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateOrEdit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrEdit(int Id)
+        {
+            return View();
+        }
+    }
+}
