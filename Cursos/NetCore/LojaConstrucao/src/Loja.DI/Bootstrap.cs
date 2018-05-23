4f5e12893af8bbb2d19ff41dfe3176da19b25e14
Adriano <amartins09@gmail.com>
2018-05-22 22:27:17 -0300

    Criar projeto de acesso ao BD e DI

---
+++ Cursos/NetCore/LojaConstrucao/src/Loja.DI/Bootstrap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Loja.Data;
+
+namespace Loja.DI
+{
+    public class Bootstrap
+    {
+        public static void Configure(IServiceCollection services, string connection){
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseMySql(connection));
+        }
+    }
+}
---
+++ Cursos/NetCore/LojaConstrucao/src/Loja.Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Loja.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace Loja.Data {
+    public class ApplicationDbContext : DbContext{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){
+
+        }
+
+        public DbSet<Category> Categories {get;set;}
+        public DbSet<Product> Products{get;set;}
+    }
+}
