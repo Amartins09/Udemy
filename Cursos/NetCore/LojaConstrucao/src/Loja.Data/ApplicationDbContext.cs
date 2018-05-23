17a6393b9e24a7a727a7d13434c4ee658fa74e3d
Adriano <amartins09@gmail.com>
2018-05-22 23:01:15 -0300

    Iniciar Migrations DB

--- Cursos/NetCore/LojaConstrucao/src/Loja.Data/ApplicationDbContext.cs
+++ Cursos/NetCore/LojaConstrucao/src/Loja.Data/ApplicationDbContext.cs
@@ -9,5 +9,12 @@ namespace Loja.Data {
 
         public DbSet<Category> Categories {get;set;}
         public DbSet<Product> Products{get;set;}
+
+        /* COMO ATUALIZAR O BANCO DE DADOS
+        dotnet ef --startup-project ..\Loja.Web\Loja.Web.csproj --project .\Loja.Data.csproj migrations add <Nome>
+
+        APLICAR A MMODIFICAÇÃO AO BANCO DE DADOS
+        dotnet ef --startup-project ..\Loja.Web\Loja.Web.csproj --project .\Loja.Data.csproj database update        
+        */
     }
 }
